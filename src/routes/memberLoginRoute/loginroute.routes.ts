f0834f4617768fc0604ffe08e039cc5de5c7b97f
Augustine Normanyo <augustine.normanyo@amalitech.com>
2024-05-18 14:29:36 +0100

    access level issues resolved updated

--- src/routes/memberLoginRoute/loginroute.routes.ts
+++ src/routes/memberLoginRoute/loginroute.routes.ts
@@ -61,7 +61,7 @@ route.post("/", async (req: Request, res: Response) => {
                         dateofbirth: user.dateofbirth,
                         gender: user.gender,
                         nationality: user.nationality,
-                        // accesslevel: accessLevelData ? {accessLevelData} : {},
+                        accesslevel: accessLevelData ? {accessLevelData} : {},
                         image: profileImageRes?.data?.data ? profileImageRes?.data?.data : ""
                     },
                     "validate12345",
