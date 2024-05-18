9f0cedbd07b9a454de736a6d4283361ccbea979e
Augustine Normanyo <augustine.normanyo@amalitech.com>
2024-05-18 12:43:20 +0100

    changed logic for member creation

--- src/routes/familyMembersRoute/memberInfo.routes.ts
+++ src/routes/familyMembersRoute/memberInfo.routes.ts
@@ -40,81 +40,81 @@ route.post("/", async(req:Request, res:Response)=>{
     let existingMember = await membersRepo.findOne({ where: { firstname: firstname, lastname: lastName } });
 
     if(existingMember){
-      res.status(401).json({
-        code:401,
-        status:false,
-        message:"Sorry Name already exist"
-      })
+      return res.status(401).json({
+        code: 401,
+        status: false,
+        message: "Sorry Name already exist"
+      });
+    }
 
-    }else{
+    let createMember = new Members();
+    createMember.firstname = firstname;
+    createMember.lastname = lastName;
+    createMember.email = email;
+    createMember.password = hashpassword;
+    createMember.gender = gender;
+    createMember.dateofbirth = dateofbirth;
+    createMember.placeofbirth = placeofbirth;
+    createMember.occupation = JSON.stringify(occupation);
+    createMember.nationality = nationality;
+    createMember.phonenumber = phonenumber;
+    createMember.mothersname = mothersname;
+    createMember.fathersname = fathersname;
+    createMember.maritalstatus = maritalstatus;
+    createMember.numberofchildren = numberofchildren;
+    createMember.primaryeducation = primaryeducation;
+    createMember.secondaryeducation = secondaryeducation;
+    createMember.tertiaryeducation = tertiaryeducation;
+    createMember.hometown = hometown;
 
-        let createMember = new Members();
-        createMember.firstname = firstname;
-        createMember.lastname = lastName;
-        createMember.email = email;
-        createMember.password = hashpassword;
-        createMember.gender = gender;
-        createMember.dateofbirth = dateofbirth;
-        createMember.placeofbirth = placeofbirth;
-        createMember.occupation = JSON.stringify(occupation);
-        createMember.nationality = nationality;
-        createMember.phonenumber = phonenumber;
-        createMember.mothersname = mothersname;
-        createMember.fathersname = fathersname;
-        createMember.maritalstatus = maritalstatus;
-        createMember.numberofchildren = numberofchildren;
-        createMember.primaryeducation = primaryeducation;
-        createMember.secondaryeducation = secondaryeducation;
-        createMember.tertiaryeducation = tertiaryeducation;
-        createMember.hometown = hometown;
+    let response = await membersRepo.save(createMember);
 
-        let response = await membersRepo.save(createMember);
+    // Send success response immediately after member creation
+    res.status(200).json({
+      code: 200,
+      message: "Member created successfully, email sending in progress",
+      data: response
+    });
 
-        
-        if(response){
-            
-           const mailtemplate = path.join(__dirname, "../../emailNotification.html");
-           const templateContent = fs.readFileSync(mailtemplate,"utf-8");
-           const emailContent = templateContent
-           .replace("{{firstname}}", firstname)
-           .replace("{{lastname}}", lastName)
-           .replace("{{password}}", password);
+    // Email sending process
+    try {
+      const mailtemplate = path.join(__dirname, "../../emailNotification.html");
+      const templateContent = fs.readFileSync(mailtemplate,"utf-8");
+      const emailContent = templateContent
+        .replace("{{firstname}}", firstname)
+        .replace("{{lastname}}", lastName)
+        .replace("{{password}}", password);
 
-           const transporter = nodemailer.createTransport({
-            service: "gmail",
-            auth: {
-              user: "augustinenormanyo98@gmail.com",
-              pass: "wiis zzwv uglo rbgo", 
-            },
-          });
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: "augustinenormanyo98@gmail.com",
+          pass: "wiis zzwv uglo rbgo", 
+        },
+      });
 
-            // Define email options
-            const mailOptions = {
-                from: "augustinenormanyo98@gmail.com",
-                to: email,
-                subject: "Account Creation Notification",
-                html: emailContent,
-            };
+      // Define email options
+      const mailOptions = {
+        from: "augustinenormanyo98@gmail.com",
+        to: email,
+        subject: "Account Creation Notification",
+        html: emailContent,
+      };
 
-            await transporter.sendMail(mailOptions);
-
-            res.status(200).json({
-                code:200,
-                message:"member created successfully email sent",
-                data:response
-            })
-        }
-
+      await transporter.sendMail(mailOptions);
+  
+    } catch (emailError) {
+      console.error("Failed to send email:", emailError);
     }
     
-}catch{
+  } catch (error) {
+    console.error("Error creating member:", error);
     res.status(500).json({
-        code:500,
-        status:false,
-        message:"Failed to send email bad gateway"
+      code: 500,
+      status: false,
+      message: "Failed to create member due to server error"
     });
-}
-
+  }
 });
 
 export default route;
